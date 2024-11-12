c9ea6c9fcf076cf89732c26766e6cb2ed889f41f
Cleriston Lima <cleriston-santos2012@hotmail.com>
2024-11-12 20:33:40 -0300

    Ajuste nos botoes do agendamento

--- obj/Debug/net8.0/DentalPlus.AssemblyInfo.cs
+++ obj/Debug/net8.0/DentalPlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DentalPlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d82ff3ef1264b8e7d736200a51edd0b3380c8b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0689cfeb33a3c2dfa02127d496f043e1ab1cbbb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DentalPlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DentalPlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
