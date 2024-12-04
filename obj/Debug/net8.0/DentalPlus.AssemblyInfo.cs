716fe6fd433e164a4e1f9faf1217c9ecbc854e2d
Cleriston Lima <cleriston-santos2012@hotmail.com>
2024-12-04 14:38:01 -0300

    Adicionando o campo de pesquisa na tela do caixa

--- obj/Debug/net8.0/DentalPlus.AssemblyInfo.cs
+++ obj/Debug/net8.0/DentalPlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DentalPlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33aa5b5420f47d648a5157f105b5291e9749a91a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8cdd08a277495807ad19aa4b7f5a988fdd2f4f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DentalPlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DentalPlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
